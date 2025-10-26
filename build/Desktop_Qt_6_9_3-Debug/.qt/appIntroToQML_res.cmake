b4f98236e71fe97e592fca249d8d16d49c00fff4
Pelevina <iuliia.pelevina@gmail.com>
2025-10-26 20:59:00 +0300

    Inital commit

---
+++ build/Desktop_Qt_6_9_3-Debug/.qt/appIntroToQML_res.cmake
@@ -0,0 +1,9 @@
+
+set(target "appIntroToQML")
+set(working_dir "/home/bug_beast/Downloads/QML-for-Beginners-Challenges-Intro-to-QML/StartingProject")
+set(src_and_dest_list
+    "/home/bug_beast/Downloads/QML-for-Beginners-Challenges-Intro-to-QML/StartingProject/IDPhoto.png"
+    "/home/bug_beast/Downloads/QML-for-Beginners-Challenges-Intro-to-QML/StartingProject/build/Desktop_Qt_6_9_3-Debug/IntroToQML/IDPhoto.png"
+
+)
+set(timestamp_file "/home/bug_beast/Downloads/QML-for-Beginners-Challenges-Intro-to-QML/StartingProject/build/Desktop_Qt_6_9_3-Debug/.qt/appIntroToQML_res.txt")
---
+++ build/Desktop_Qt_6_9_3-Debug/qtcsettings.cmake
@@ -0,0 +1,12 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/bin/clang-18" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/bin/clang++-18" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/bug_beast/Qt/6.9.3/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "/home/bug_beast/Qt/MaintenanceTool" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/bug_beast/Downloads/QML-for-Beginners-Challenges-Intro-to-QML/StartingProject/build/Desktop_Qt_6_9_3-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/bug_beast/Qt/6.9.3/gcc_64" CACHE "PATH" "" FORCE)
