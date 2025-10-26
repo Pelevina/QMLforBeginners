8974b1c51407dce8fbcc298f87ab23baf02d5a4c
Pelevina <iuliia.pelevina@gmail.com>
2025-10-26 21:04:36 +0300

    Delete build

--- build/Desktop_Qt_6_9_3-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appIntroToQML_FILE /home/bug_beast/Downloads/QML-for-Beginners-Challenges-Intro-to-QML/StartingProject/build/Desktop_Qt_6_9_3-Debug/appIntroToQML)
-set(__QT_DEPLOY_TARGET_appIntroToQML_TYPE EXECUTABLE)
